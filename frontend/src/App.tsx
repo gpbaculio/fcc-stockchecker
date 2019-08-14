a77a2ea54091e544c4aa7ad2501d13423bc0a4b7
Glendon Philipp <gpbaculio@gmail.com>
2019-08-14 18:20:57 +0800

    fix types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
-import { bisector } from 'd3-array';
+import { Input } from 'reactstrap';
 import './App.css';
 import * as d3 from 'd3';
 import Header from './components/Header';
 
-class App extends Component {
-  state = {
-    metaData: {},
+interface tsData {
+  '1. open': string;
+  '2. high': string;
+  '3. low': string;
+  '4. close': string;
+  '5. volume': string;
+}
+
+interface AppState {
+  timeSeries: {
+    [text: string]: tsData;
+  };
+  metaData: {
+    '1. Information': string;
+    '2. Symbol': string;
+    '3. Last Refreshed': string;
+    '4. Interval': string;
+    '5. Output Size': string;
+    '6. Time Zone': string;
+  };
+}
+
+class App extends Component<{}, AppState> {
+  public readonly state: Readonly<AppState> = {
+    metaData: {
+      '1. Information': '',
+      '2. Symbol': '',
+      '3. Last Refreshed': '',
+      '4. Interval': '',
+      '5. Output Size': '',
+      '6. Time Zone': ''
+    },
     timeSeries: {}
   };
   componentDidMount = async () => {
@@ -22,8 +50,8 @@ class App extends Component {
     );
   };
   createChart = () => {
-    var margin = { top: 30, right: 20, bottom: 30, left: 50 },
-      width = 550 - margin.left - margin.right,
+    var margin = { top: 30, right: 50, bottom: 30, left: 50 },
+      width = 650 - margin.left - margin.right,
       height = 470 - margin.top - margin.bottom;
     // Set the dimensions of the canvas / graph
     var svg = d3
@@ -33,57 +61,63 @@ class App extends Component {
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    const { timeSeries }: any = this.state;
+    const { timeSeries } = this.state;
 
     const [date1, date2] = Object.keys(timeSeries)
       .map(ts => ts.split(' ')[0])
       .filter((value, index, self) => self.indexOf(value) === index)
       .map(d => new Date(d));
+
     const dateForChart = date1.getTime() > date2.getTime() ? date1 : date2;
-    let timesData: any = {};
-    const times = Object.keys(timeSeries).reduce((acc: Date[], val) => {
+
+    let timesData: {
+      [text: string]: tsData;
+    } = {};
+
+    Object.keys(timeSeries).forEach(val => {
       const valDate = new Date(val.split(' ')[0]);
       if (dateForChart.getTime() === valDate.getTime()) {
-        acc.push(new Date(val));
         timesData[val] = timeSeries[val];
       }
-      return acc;
-    }, []);
+    });
+
     const timesDataPath = Object.keys(timesData).map(td => ({
       ...timesData[td],
       date: new Date(td)
     }));
-    var xDomain = d3.extent(timesDataPath, function(d) {
-      return d.date;
-    });
-    var yDomain = d3.extent(timesDataPath, function(d) {
-      return d['4. close'];
-    });
-    const closePrices = Object.keys(timesData).map(
-      td => timesData[td]['4. close']
+
+    const xDomain = d3.extent(timesDataPath, ({ date }) => date);
+
+    const closePrices: number[] = Object.keys(timesData).map(td =>
+      Number(timesData[td]['4. close'])
     );
-    const closeMax = Math.round(d3.max(closePrices) / 100) * 100;
-    const closeMin = Math.round(d3.min(closePrices) / 100) * 100 - 100;
+
+    const closeMax = Math.round((d3.max(closePrices) as number) / 100) * 100;
+    const closeMin =
+      Math.round((d3.min(closePrices) as number) / 100) * 100 - 100;
+
     const yScale = d3
       .scaleLinear()
       .domain([closeMin, closeMax])
       .range([height, 0]);
-    var xScale = d3
+
+    const xScale = d3
       .scaleTime()
       .range([0, width])
-      .domain([...xDomain]);
+      .domain([...(xDomain as Date[])]);
 
-    var xAxis = d3.axisBottom(xScale);
-    var yAxis = d3.axisLeft(yScale);
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
 
-    var line = d3
-      .line()
-      .x(function(d: any) {
-        return xScale(d.date);
+    const line = d3
+      .line<tsData & { date: Date }>()
+      .x(d => {
+        return xScale(d.date.getTime());
       })
-      .y(function(d: any) {
-        return yScale(d['4. close']);
+      .y(d => {
+        return yScale(Number(d['4. close']));
       });
+
     svg
       .append('g')
       .attr('class', 'grid')
@@ -117,12 +151,12 @@ class App extends Component {
         return xScale(d.date);
       })
       .attr('cy', function(d) {
-        return yScale(d['4. close']);
+        return yScale(Number(d['4. close']));
       })
       .attr('r', 3)
       .attr('class', 'circle');
-    var focus = g.append('g').style('display', 'none');
-    var focusText = svg
+    const focus = g.append('g').style('display', 'none');
+    const focusText = svg
       .append('g')
       .append('text')
       .style('opacity', 0)
@@ -142,9 +176,7 @@ class App extends Component {
       .attr('r', 3)
       .attr('class', 'circle focusCircle');
 
-    var bisect = d3.bisector(function(d: any) {
-      return d.date;
-    }).left;
+    const bisect = d3.bisector((d: tsData & { date: Date }) => d.date).left;
 
     g.append('rect')
       .attr('class', 'overlay')
@@ -152,7 +184,7 @@ class App extends Component {
       .style('pointer-events', 'all')
       .attr('width', width)
       .attr('height', height)
-      .on('mouseover', function() {
+      .on('mouseover', () => {
         focus.style('display', null);
         focusText
           .style('opacity', 1)
@@ -160,25 +192,32 @@ class App extends Component {
           .style('background-size', '#fff')
           .style('z-index', '99');
       })
-      .on('mouseout', function() {
+      .on('mouseout', () => {
         focus.style('display', 'none');
         focusText.style('opacity', 0);
       })
       .on('mousemove', () => {
         var mouse = d3.mouse(d3.event.target);
-        var mouseDate: any = xScale.invert(mouse[0]);
-        const sortDate = timesDataPath.sort(function(a: any, b: any) {
-          return a.date - b.date;
+        var mouseDate: Date = xScale.invert(mouse[0]);
+        const sortDate = timesDataPath.sort(function(
+          a: tsData & { date: Date },
+          b: tsData & { date: Date }
+        ) {
+          return a.date.getTime() - b.date.getTime();
         });
         var i = bisect(sortDate, mouseDate, 1); // returns the index to the current data item
 
         var d0 = timesDataPath[i - 1];
         var d1 = timesDataPath[i];
         // work out which date value is closest to the mouse
-        var d = mouseDate - d0.date > d1.date - mouseDate ? d1 : d0;
+        var d =
+          mouseDate.getTime() - d0.date.getTime() >
+          d1.date.getTime() - mouseDate.getTime()
+            ? d1
+            : d0;
 
         var x = xScale(d.date);
-        var y = yScale(d['4. close']);
+        var y = yScale(Number(d['4. close']));
 
         focus
           .select('#focusCircle')
@@ -203,6 +242,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Header />
+        <Input />
         <div className='svg-container' />
       </div>
     );
