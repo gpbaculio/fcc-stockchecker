8a2cd5fc6fd830d7f3bc0b4915ca9846110c06a1
Glendon Philipp <gpbaculio@gmail.com>
2019-08-19 20:47:03 +0800

    ready to deploy

--- backend/lib/app.ts
+++ backend/lib/app.ts
@@ -54,11 +54,12 @@ class App {
     sessionConfig.cookie.secure = true; // serve secure cookies
 
     this.app.use(session(sessionConfig));
-    // this.app.use(express.static(path.join(__dirname, '../../frontend/build')));
-    // // Handle React routing, return all requests to React app
-    // this.app.get('/*', (_req, res) => {
-    //   res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
-    // });
+    // Serve any static files
+    this.app.use(express.static(path.join(__dirname, '../../frontend/build')));
+    // Handle React routing, return all requests to React app
+    this.app.get('/*', (_req, res) =>
+      res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'))
+    );
 
     this.fccTestingRoute.routes(this.app);
     this.stockCheckerRoute.routes(this.app);
